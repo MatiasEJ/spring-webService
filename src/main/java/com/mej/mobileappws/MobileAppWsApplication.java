499efcda0ec4eafd5843d54c183bdccc11a01cc7
MeJ <don.juncos@gmail.com>
2021-02-26 18:13:21 -0300

    cleaning pom

--- src/main/java/com/mej/mobileappws/MobileAppWsApplication.java
+++ src/main/java/com/mej/mobileappws/MobileAppWsApplication.java
@@ -11,12 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import javax.swing.*;
 @SpringBootApplication
 public class MobileAppWsApplication  {
-//extends SpringBootServletInitializer for Deploy on AWS
-
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//		return builder.sources(MobileAppWsApplication.class);
-//	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
